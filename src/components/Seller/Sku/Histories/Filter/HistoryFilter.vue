1684c957b5edf57080a8c2f39af73e54002e35e5
zohreh <zohreh.tt@gmail.com>
2023-11-27 14:03:06 +0330

    fix(seller history): Fix seller sku history

---
+++ src/components/Seller/Sku/Histories/Filter/HistoryFilter.vue
@@ -0,0 +1,277 @@
+<template>
+<div class="text-center c-modal-table-filter">
+    <v-btn
+        @click="openModal()"
+        variant="outlined"
+        rounded
+        class="px-3 mt-1 mr-4">
+        <template v-slot:prepend>
+            <img src="@/assets/img/Fillter.svg" alt="shavaz-img">
+        </template>
+        فیلتر
+    </v-btn>
+
+    <v-dialog
+        v-if="dialog"
+        v-model="dialog"
+        width="1000">
+        <v-card>
+            <header class="modal__header d-flex justify-center align-center">
+                <span class="t16400 pa-6">
+                    فیلتر پیشرفته
+                </span>
+
+                <v-btn
+                    class="modal__header__btn"
+                    @click="closeModal()"
+                    variant="icon">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </header>
+            <div class="mb-5 px-5">
+                <v-divider />
+            </div>
+
+            <div>
+                <v-row
+                    justify="end"
+                    align="center"
+                    class="px-10 d--rtl">
+                    <template v-for="(filter, index) in filterField" :key="index">
+                        <!-- Text filds -->
+                        <v-col v-if="filter.type === 'text'" cols="6">
+                            <div class="t12300 text-right mb-1 text-grey">{{filter.name}}</div>
+
+                            <v-text-field
+                                variant="outlined"
+                                :placeholder="filter.placeHolder"
+                                :name="filter.value "
+                                v-model="values[index].value" />
+                        </v-col>
+                    </template>
+                </v-row>
+            </div>
+
+            <div class="mt-3 mb-5 px-5">
+                <v-divider />
+            </div>
+
+            <v-row
+                justify="center"
+                align="center"
+                class="px-5 p1-2">
+                <v-col cols="6">
+                    <v-btn
+                        @click="removeAllFilters()"
+                        variant="text"
+                        height="40"
+                        rounded
+                        class="px-5 mr-5">
+                        حذف همه فیلترها
+                    </v-btn>
+                </v-col>
+
+                <v-col cols="6">
+                    <div class="text-right">
+                        <v-btn
+                            @click="setFilter()"
+                            color="primary500"
+                            height="40"
+                            rounded
+                            class="px-5 mr-5">
+                            اعمال تغییرات
+                        </v-btn>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-dialog>
+</div>
+</template>
+
+<script>
+import {
+    SkuSellerPanelFilter
+} from "@/assets/js/filter_sku_to_seller";
+
+export default {
+    props: {
+        filterField: [],
+        path: '',
+        brandsList: [],
+        colorsList: [],
+        categoriesList: [],
+        disableCategory: false,
+        skuGroupList: [],
+        model: ''
+    },
+
+    data() {
+        return {
+            dialog: false,
+            active: false,
+            values: [],
+        }
+    },
+
+    computed: {
+        customer_price_from() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'customer_price_from');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        customer_price_to() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'customer_price_to');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        site_price_from() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'site_price_from');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        site_price_to() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'site_price_to');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        base_discount_from() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'base_discount_from');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        base_discount_to() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'base_discount_to');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        marketing_discount_from() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'marketing_discount_from');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+        marketing_discount_to() {
+            try {
+                const labelObject = this.values.find(element => element.name === 'marketing_discount_to');
+                return labelObject.value
+            } catch (error) {
+                return ''
+            }
+        },
+    },
+
+    methods: {
+
+        setFilter() {
+            const filter = new SkuSellerPanelFilter()
+
+            if (this.base_discount_from) {
+                filter.base_discount_from = this.base_discount_from
+            } else {
+                filter.base_discount_from = null
+            }
+
+            if (this.base_discount_to) {
+                filter.base_discount_to = this.base_discount_to
+            } else {
+                filter.base_discount_to = null
+            }
+
+            if (this.marketing_discount_from) {
+                filter.marketing_discount_from = this.marketing_discount_from
+            } else {
+                filter.marketing_discount_from = null
+            }
+
+            if (this.marketing_discount_to) {
+                filter.marketing_discount_to = this.marketing_discount_to
+            } else {
+                filter.marketing_discount_to = null
+            }
+
+            if (this.site_price_from) {
+                filter.site_price_from = this.site_price_from
+            } else {
+                filter.site_price_from = null
+            }
+
+            if (this.site_price_to) {
+                filter.site_price_to = this.site_price_to
+            } else {
+                filter.site_price_to = null
+            }
+
+            if (this.customer_price_from) {
+                filter.customer_price_from = this.customer_price_from
+            } else {
+                filter.customer_price_from = null
+            }
+
+            if (this.customer_price_to) {
+                filter.customer_price_to = this.customer_price_to
+            } else {
+                filter.customer_price_to = null
+            }
+
+            filter.page = 1;
+            filter.per_page = this.$route.query.per_page;
+            this.$router.push('/' + this.path + '/' + filter.query_maker());
+            this.dialog = false;
+        },
+
+        removeAllFilters() {
+            this.$router.push('/' + this.path);
+            this.values = [];
+
+            this.filterField.forEach(el => {
+                const form = {
+                    name: el.value,
+                    value: ''
+                }
+
+                this.values.push(form)
+                this.dialog = false;
+            })
+        },
+
+        openModal() {
+            this.dialog = true;
+        },
+
+        closeModal() {
+            this.dialog = false;
+        },
+    },
+
+    mounted() {
+        this.filterField.forEach(el => {
+            const form = {
+                name: el.value,
+                value: null
+            }
+            this.values.push(form)
+        })
+    }
+}
+</script>
--- src/components/Seller/Sku/Histories/PriceHistoryList.vue
+++ src/components/Seller/Sku/Histories/PriceHistoryList.vue
@@ -1,187 +1,180 @@
 <template>
-  <div class="h-100 d-flex flex-column align-stretch seller">
+<div class="h-100 d-flex flex-column align-stretch seller">
     <v-card height="70" class="ma-5 br-12 stretch-card-header-70">
-      <v-row
-          justify="center"
-          align="center"
-          class="px-10 py-5">
-        <v-col cols="6">
-          <div class="d-flex justify-start align-center">
-            <div>
-              <span>
-                شناسه کالا :
-              </span>
+        <v-row
+            justify="center"
+            align="center"
+            class="px-10 py-5">
+            <v-col cols="6">
+                <div class="d-flex justify-start align-center pt-1">
+                    <div>
+                        <span class="t12500">
+                            شناسه کالا :
+                        </span>
 
-              <span>
-                {{sku?.id}}
-              </span>
-            </div>
-            <div class="mr-5">
-              <span>
-               نام کالا :
-              </span>
+                        <span class="t12500 text-grey">
+                            {{sku?.id}}
+                        </span>
+                    </div>
+                    <div class="mr-5">
+                        <span class="t12500">
+                            نام کالا :
+                        </span>
 
-              <span>
-                {{sku?.label}}
-              </span>
-            </div>
-          </div>
-        </v-col>
+                        <span class="t12500 text-grey">
+                            {{sku?.label}}
+                        </span>
+                    </div>
+                </div>
+            </v-col>
 
-        <v-col cols="6">
-          <v-row justify="end">
-            <ModalColumnFilter :changeHeaderShow="changeHeaderShow" :header="headerPriceHistory" />
+            <v-col cols="6">
+                <v-row justify="end">
+                    <ModalColumnFilter :changeHeaderShow="changeHeaderShow" :header="headerPriceHistory" />
 
-            <ModalTableFilter :path="`seller/sku/${$route.params.sellerId}/history/price/${$route.params.skuId}`" :filterField="filterPriceHistory" />
-          </v-row>
-        </v-col>
-      </v-row>
+                    <ModalTableFilter :path="`seller/sku/${$route.params.sellerId}/history/price/${$route.params.skuId}`" :filterField="filterPriceHistory" />
+                </v-row>
+            </v-col>
+        </v-row>
     </v-card>
 
     <v-card class="ma-5 mt-0 br-12 flex-grow-1 d-flex flex-column align-stretch" height="580">
-      <Table
-          class="flex-grow-1"
-          :header="headerPriceHistory"
-          :items="priceHistory.data"
-          :page="page"
-          :perPage="dataTableLength"
-          editUrl="/seller/edit/"
-          activePath="seller/crud/update/activation/"
-          changeStatusUrl="seller/crud/update/contract/"
-          :loading="loading"
-          @updateList = "updateList"
-          updateUrl="seller/csv/mass-update"
-          model="price" />
+        <Table
+            class="flex-grow-1"
+            :header="headerPriceHistory"
+            :items="priceHistory.data"
+            :page="page"
+            :perPage="dataTableLength"
+            editUrl="/seller/edit/"
+            activePath="seller/crud/update/activation/"
+            changeStatusUrl="seller/crud/update/contract/"
+            :loading="loading"
+            @updateList="updateList"
+            updateUrl="seller/csv/mass-update"
+            model="price" />
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions class="pb-3">
-        <v-row class="px-8">
-          <v-col cols="3" class="d-flex justify-start">
-            <ModalExcelDownload :getEndPoint="`seller/${$route.params.sellerId}/sku/${$route.params.skuId}/history/price/csv/get/export`" />
-          </v-col>
+        <v-card-actions class="pb-3">
+            <v-row class="px-8">
+                <v-col cols="3" class="d-flex justify-start">
+                    <ModalExcelDownload :getEndPoint="`seller/${$route.params.sellerId}/sku/${$route.params.skuId}/history/price/csv/get/export`" />
+                </v-col>
 
-          <v-col cols="6" class="d-flex justify-center">
-            <div class="text-center">
-              <v-pagination
-                  v-model="priceHistoryPage"
-                  :length="pageLength"
-                  rounded="circle"
-                  size="40"
-                  :total-visible="4"
-                  prev-icon="mdi-chevron-right"
-                  next-icon="mdi-chevron-left" />
-            </div>
-          </v-col>
+                <v-col cols="6" class="d-flex justify-center">
+                    <div class="text-center">
+                        <v-pagination
+                            v-model="priceHistoryPage"
+                            :length="pageLength"
+                            rounded="circle"
+                            size="40"
+                            :total-visible="4"
+                            prev-icon="mdi-chevron-right"
+                            next-icon="mdi-chevron-left" />
+                    </div>
+                </v-col>
 
-          <v-col cols="3" class="d-flex justify-end">
-            <div
-                align="center"
-                id="rowSection"
-                class="d-flex align-center">
+                <v-col cols="3" class="d-flex justify-end">
+                    <div
+                        align="center"
+                        id="rowSection"
+                        class="d-flex align-center">
                         <span class="ml-5">
                             تعداد سطر در هر صفحه
                         </span>
-              <span class="mt-2" id="row-selector">
+                        <span class="mt-2" id="row-selector">
                             <v-select
                                 v-model="dataTableLength"
                                 class="t1330"
                                 variant="outlined"
                                 :items="[25,50,100]" />
                         </span>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card-actions>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card-actions>
     </v-card>
-  </div>
+</div>
 </template>
 
 <script>
 import Table from '@/components/Seller/Sku/Histories/Table/HistoriesTable.vue'
 import Seller from "@/composables/Seller";
 import Sku from "@/composables/Sku";
-import ModalTableFilter from '@/components/Seller/Sku/Filter/SkuSellerFilter.vue'
+import ModalTableFilter from '@/components/Seller/Sku/Histories/Filter/HistoryFilter.vue'
 import ModalColumnFilter from '@/components/Public/ModalColumnFilter.vue'
 import ModalExcelDownload from "@/components/Public/ModalExcelDownload.vue";
-import { openToast} from "@/assets/js/functions";
+import {
+    openToast
+} from "@/assets/js/functions";
 export default {
-  setup(props) {
-    const {getSkue , sku } = new Sku()
-    const {
-      pageLength,
-      priceHistory,
-      filterPriceHistory,
-      dataTableLength,
-      priceHistoryPage,
-      headerPriceHistory,
-      addPagination,
-      addPerPage,
-      loading,
-      getPriceHistory
-    } = Seller();
-    return {
-      pageLength,
-      priceHistory,
-      filterPriceHistory,
-      dataTableLength,
-      priceHistoryPage,
-      headerPriceHistory,
-      addPagination,
-      addPerPage,
-      loading,
-      getPriceHistory,
-      getSkue , sku
-    };
-  },
-
-  components: {
-    Table,
-    ModalTableFilter,
-    ModalColumnFilter,
-    ModalExcelDownload,
-  },
-
-  computed: {
-    confirmModal() {
-      return this.$store.getters['get_confirmForm'].confirmModal
-    }
-  },
-
-  methods: {
-    changeHeaderShow(index, value) {
-      this.header[index].show = value
+    setup(props) {
+        const {
+            getSkue,
+            sku
+        } = new Sku()
+        const {
+            pageLength,
+            priceHistory,
+            filterPriceHistory,
+            dataTableLength,
+            priceHistoryPage,
+            headerPriceHistory,
+            addPagination,
+            addPerPage,
+            loading,
+            getPriceHistory
+        } = Seller();
+        return {
+            pageLength,
+            priceHistory,
+            filterPriceHistory,
+            dataTableLength,
+            priceHistoryPage,
+            headerPriceHistory,
+            addPagination,
+            addPerPage,
+            loading,
+            getPriceHistory,
+            getSkue,
+            sku
+        };
     },
 
-    updateList(status){
-      if(status === 'true'){
-        this.getSellerList();
-      }
+    components: {
+        Table,
+        ModalTableFilter,
+        ModalColumnFilter,
+        ModalExcelDownload,
     },
-  },
 
-  mounted() {
-    this.getPriceHistory()
-    this.getSkue()
-  },
-
-  watch: {
-    dataTableLength(val) {
-      this.addPerPage(val)
-    },
-    confirmModal(val) {
-      if (this.$cookies.get('deleteItem')) {
-        if (!val) {
-          this.getSellerList();
-          openToast(
-              this.$store,
-              'انبار با موفقیت حذف شد',
-              "success"
-          );
-          this.$cookies.remove('deleteItem')
+    computed: {
+        confirmModal() {
+            return this.$store.getters['get_confirmForm'].confirmModal
         }
-      }
     },
-  }
+
+    methods: {
+        changeHeaderShow(index, value) {
+            this.headerPriceHistory[index].show = value
+        },
+
+        updateList(status) {
+            if (status === 'true') {
+                this.getPriceHistory();
+            }
+        },
+    },
+
+    mounted() {
+        this.getPriceHistory()
+        this.getSkue()
+    },
+
+    watch: {
+        dataTableLength(val) {
+            this.addPerPage(val)
+        },
+    }
 }
 </script>
