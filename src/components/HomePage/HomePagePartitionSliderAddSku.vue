afe8bd790795507dcdf8e1eff1e00bd3c4655a90
zohreh <zohreh.tt@gmail.com>
2023-12-29 14:14:34 +0330

    feat(Pricing): Add pricing pages

--- src/components/HomePage/HomePagePartitionSliderAddSku.vue
+++ src/components/HomePage/HomePagePartitionSliderAddSku.vue
@@ -1,103 +1,92 @@
 <template>
-  <div class="h-100 d-flex flex-column align-stretch sku-seller">
+<div class="h-100 d-flex flex-column align-stretch sku-seller">
 
-    <v-card height="70" class="ma-5 br-12 stretch-card-header-70" >
-      <v-row
-          justify="start"
-          align="center"
-          class="px-10 py-5">
-        <v-col cols="6">
-          <v-row justify="start">
-            <v-autocomplete
-                placeholder="نام کالا یا شماره SKU را جستجو نمایید"
-                variant="outlined"
-                prepend-inner-icon-cb="mdi-map-marker"
-                rounded="lg"
-                :items="skuSearchList"
-                item-title="id"
-                return-object
-                v-debounce:1s.unlock="searchSku">
+    <v-card height="70" class="ma-5 br-12 stretch-card-header-70">
+        <v-row
+            justify="start"
+            align="center"
+            class="px-10 py-5">
+            <v-col cols="6">
+                <v-row justify="start">
+                    <v-autocomplete
+                        placeholder="نام کالا یا شماره SKU را جستجو نمایید"
+                        variant="outlined"
+                        prepend-inner-icon-cb="mdi-map-marker"
+                        rounded="lg"
+                        :items="skuSearchList"
+                        item-title="id"
+                        return-object
+                        v-debounce:1s.unlock="searchSku">
 
-              <template v-slot:item="item">
-                <v-list-item>
-                  <v-row justify="center">
+                        <template v-slot:item="item">
+                            <v-list-item>
+                                <v-row justify="center">
+                                    <v-col cols="4">
+                                        <div @click="assignSku(item.props.value.id,item.props.value.sku_id)" class="seller__add-sku-btn d-flex justify-center align-center">
+                                            <v-icon>mdi-plus</v-icon>
+                                        </div>
+                                    </v-col>
 
-                    <v-col cols="4">
-
-                      <div @click="assignSku(item.props.value.id,item.props.value.sku_id)" class="seller__add-sku-btn d-flex justify-center align-center">
-                        <v-icon>mdi-plus</v-icon>
-                      </div>
-
-                    </v-col>
-                    <v-col cols="8">
-                      <text-clamp
-                          :text='item?.props?.title'
-                          :max-lines='1'
-                          autoResize
-                          location="start"
-                          class="text-gray500 t14300 text-right" />
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-              </template>
-            </v-autocomplete>
-
-          </v-row>
-        </v-col>
-
-
-
-      </v-row>
+                                    <v-col cols="8">
+                                        <text-clamp
+                                            :text='item?.props?.title'
+                                            :max-lines='1'
+                                            autoResize
+                                            location="start"
+                                            class="text-gray500 t14300 text-right" />
+                                    </v-col>
+                                </v-row>
+                            </v-list-item>
+                        </template>
+                    </v-autocomplete>
+                </v-row>
+            </v-col>
+        </v-row>
     </v-card>
 
     <v-card class="ma-5 br-12 flex-grow-1 d-flex flex-column align-stretch" height="580">
-      <Table
-          class="flex-grow-1"
-          :header="homePageSkuHeader"
-          :items="homeSinglePartitionSlider?.skus"
+        <Table
+            class="flex-grow-1"
+            :header="homePageSkuHeader"
+            :items="homeSinglePartitionSlider?.skus"
+            :updateSkuUrl="`page/home/section/slider/${homeSinglePartitionSlider?.slider_id}/sku/attach`"
+            activePath="seller/crud/update/activation/"
+            :deletePath="`page/home/section/${$route.params.sectionId}/sku/detach/`"
+            changeStatusUrl="seller/crud/update/contract/"
+            :loading="loading"
+            @updateList="updateList"
+            updateUrl="seller/csv/mass-update"
+            model="partition" />
 
-          :updateSkuUrl="`page/home/section/slider/${homeSinglePartitionSlider?.slider_id}/sku/attach`"
-          activePath="seller/crud/update/activation/"
-          :deletePath="`page/home/section/${$route.params.sectionId}/sku/detach/`"
-          changeStatusUrl="seller/crud/update/contract/"
-          :loading="loading"
-          @updateList="updateList"
-          updateUrl="seller/csv/mass-update"
-          model="partition" />
+        <v-divider />
 
-      <v-divider />
+        <v-card-actions class="pb-3">
+            <v-row class="px-8">
+                <v-col cols="3" class="d-flex justify-start" />
 
-      <v-card-actions class="pb-3">
-        <v-row class="px-8">
-          <v-col cols="3" class="d-flex justify-start">
+                <v-col cols="6" class="d-flex justify-center" />
 
-          </v-col>
-
-          <v-col cols="6" class="d-flex justify-center">
-
-          </v-col>
-
-          <v-col cols="3" class="d-flex justify-end">
-            <div
-                align="center"
-                id="rowSection"
-                class="d-flex align-center">
+                <v-col cols="3" class="d-flex justify-end">
+                    <div
+                        align="center"
+                        id="rowSection"
+                        class="d-flex align-center">
                         <span class="ml-5">
                             تعداد سطر در هر صفحه
                         </span>
-              <span class="mt-2" id="row-selector">
+                        <span class="mt-2" id="row-selector">
                             <v-select
                                 v-model="dataSkuTableLength"
                                 class="t1330"
                                 variant="outlined"
                                 :items="[25,50,100]" />
                         </span>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card-actions>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card-actions>
     </v-card>
-  </div>
+</div>
 </template>
 
 <script>
@@ -109,180 +98,196 @@ import ModalColumnFilter from '@/components/Public/ModalColumnFilter.vue'
 import ModalGroupAdd from '@/components/Public/ModalGroupAdd.vue'
 import ModalExcelDownload from "@/components/Public/ModalExcelDownload.vue";
 import {
-  openToast
+    openToast
 } from "@/assets/js/functions";
 import {
-  AxiosCall
+    AxiosCall
 } from "@/assets/js/axios_call";
-import Brands from "@/composables/Brands";
-import Colors from "@/composables/Colors";
-import Categories from "@/composables/Categories";
-import Sku from "@/composables/Sku";
 export default {
-  setup(props) {
-    const {
-      getSkuSeller , sellerSku,headerSku,
-      filterFieldSku ,skuPage ,dataSkuTableLength,addSkuPerPage,pageLength
-    } = Seller();
-    const {
-      getHomeSection , homeSection , homePageSkuHeader , getHomeSinglePartitionSlider , homeSinglePartitionSlider
-    } = new Home()
-    return {
-      getSkuSeller , sellerSku,headerSku,filterFieldSku,addSkuPerPage , pageLength,
-      skuPage ,dataSkuTableLength ,  getHomeSection , homeSection  , homePageSkuHeader , getHomeSinglePartitionSlider , homeSinglePartitionSlider
-    };
-  },
-
-  data(){
-    return{
-      skuSearchList:[],
-      title:'',
-      editLoading:false
-    }
-  },
-
-  components: {
-    Table,
-    ModalGroupAdd,
-    SkuModalTableFilter,
-    ModalColumnFilter,
-    ModalExcelDownload,
-  },
-
-  computed: {
-    confirmModal() {
-      return this.$store.getters['get_confirmForm'].confirmModal
-    },
-    skuGroupList() {
-      try {
-        let group = []
-        this.allSkuGroups.data.forEach(skuGroup => {
-          const form = {
-            label: skuGroup.label,
-            value: skuGroup.id
-          }
-          group.push(form)
-        })
-        return group
-      } catch (e) {
-        return []
-      }
-    },
-    skuList() {
-      try {
-        let sku = []
-        this.skuSearchList.forEach(permission => {
-          const form = {
-            name: permission.label + '(' + permission.id + ')',
-            id: permission.id
-          }
-          sku.push(form)
-        })
-        return sku
-      } catch (e) {
-        return []
-      }
-    },
-  },
-
-  methods: {
-    async editSection() {
-      this.editLoading = true
-      let formData = new FormData();
-      const AxiosMethod = new AxiosCall()
-      AxiosMethod.end_point = `page/home/section/crud/update/${this.$route.params.sectionId}`
-
-      formData.append('label', this.title)
-      AxiosMethod.form = formData
-      AxiosMethod.store = this.$store
-      AxiosMethod.using_auth = true
-      AxiosMethod.token = this.$cookies.get('adminToken')
-      let data = await AxiosMethod.axios_post()
-      if (data) {
-        this.editLoading = false
-        this.dialog = false
-      } else {
-        this.editLoading = false
-      }
-
-    },
-    changeHeaderShow(index, value) {
-      this.headerSku[index].show = value
+    setup(props) {
+        const {
+            getSkuSeller,
+            sellerSku,
+            headerSku,
+            filterFieldSku,
+            skuPage,
+            dataSkuTableLength,
+            addSkuPerPage,
+            pageLength
+        } = Seller();
+        const {
+            getHomeSection,
+            homeSection,
+            homePageSkuHeader,
+            getHomeSinglePartitionSlider,
+            homeSinglePartitionSlider
+        } = new Home()
+        return {
+            getSkuSeller,
+            sellerSku,
+            headerSku,
+            filterFieldSku,
+            addSkuPerPage,
+            pageLength,
+            skuPage,
+            dataSkuTableLength,
+            getHomeSection,
+            homeSection,
+            homePageSkuHeader,
+            getHomeSinglePartitionSlider,
+            homeSinglePartitionSlider
+        };
     },
 
-    updateList(status) {
-      console.log('3.skuList', status)
-      if (status === 'true') {
-        this.getSkuSeller();
-      }
-    },
-
-    async searchSku(search) {
-      this.skuSearchList = []
-
-      const AxiosMethod = new AxiosCall()
-      AxiosMethod.using_auth = true
-      AxiosMethod.token = this.$cookies.get('adminToken')
-      AxiosMethod.end_point = `seller/sku/search?id=${search}`
-      let data = await AxiosMethod.axios_get()
-      if (data) {
-        this.skuSearchList = data.data.data
-      }
-    },
-
-    async assignSku(id, skuId) {
-      const formData = new FormData()
-      const AxiosMethod = new AxiosCall()
-      AxiosMethod.using_auth = true
-      AxiosMethod.store = this.$store
-      AxiosMethod.token = this.$cookies.get('adminToken')
-      AxiosMethod.end_point = `page/home/section/slider/${this.homeSinglePartitionSlider.slider_id}/sku/attach`
-      formData.append('shps', id)
-      formData.append('sku_id', skuId)
-      formData.append('priority', 1)
-      formData.append('partition_id',this.$route.params.partitionId )
-      AxiosMethod.form = formData
-      let data = await AxiosMethod.axios_post()
-      if (data) {
-        this.getHomeSinglePartitionSlider()
-        openToast(
-            this.$store,
-            'کد کالا با موفقیت افزوده شد.',
-            "success"
-        );
-      }
-    }
-  },
-
-  mounted() {
-    this.getHomeSinglePartitionSlider()
-  },
-
-  watch: {
-    homeSection(val){
-      this.title = val.label
-    },
-    dataSkuTableLength(val) {
-      this.addSkuPerPage(val)
-    },
-    confirmModal(val) {
-      if (localStorage.getItem('deleteObject') === 'done') {
-        if (!val) {
-          this.getHomeSinglePartitionSlider();
-          openToast(
-              this.$store,
-              'محصول با موفقیت حذف شد',
-              "success"
-          );
-          localStorage.removeItem('deleteObject')
+    data() {
+        return {
+            skuSearchList: [],
+            title: '',
+            editLoading: false
         }
-      }
     },
-    $route(to, from) {
-      this.getHomeSection()
-    }
-  }
 
+    components: {
+        Table,
+        ModalGroupAdd,
+        SkuModalTableFilter,
+        ModalColumnFilter,
+        ModalExcelDownload,
+    },
+
+    computed: {
+        confirmModal() {
+            return this.$store.getters['get_confirmForm'].confirmModal
+        },
+        skuGroupList() {
+            try {
+                let group = []
+                this.allSkuGroups.data.forEach(skuGroup => {
+                    const form = {
+                        label: skuGroup.label,
+                        value: skuGroup.id
+                    }
+                    group.push(form)
+                })
+                return group
+            } catch (e) {
+                return []
+            }
+        },
+        skuList() {
+            try {
+                let sku = []
+                this.skuSearchList.forEach(permission => {
+                    const form = {
+                        name: permission.label + '(' + permission.id + ')',
+                        id: permission.id
+                    }
+                    sku.push(form)
+                })
+                return sku
+            } catch (e) {
+                return []
+            }
+        },
+    },
+
+    methods: {
+        async editSection() {
+            this.editLoading = true
+            let formData = new FormData();
+            const AxiosMethod = new AxiosCall()
+            AxiosMethod.end_point = `page/home/section/crud/update/${this.$route.params.sectionId}`
+
+            formData.append('label', this.title)
+            AxiosMethod.form = formData
+            AxiosMethod.store = this.$store
+            AxiosMethod.using_auth = true
+            AxiosMethod.token = this.$cookies.get('adminToken')
+            let data = await AxiosMethod.axios_post()
+            if (data) {
+                this.editLoading = false
+                this.dialog = false
+            } else {
+                this.editLoading = false
+            }
+
+        },
+        changeHeaderShow(index, value) {
+            this.headerSku[index].show = value
+        },
+
+        updateList(status) {
+            console.log('3.skuList', status)
+            if (status === 'true') {
+                this.getSkuSeller();
+            }
+        },
+
+        async searchSku(search) {
+            this.skuSearchList = []
+
+            const AxiosMethod = new AxiosCall()
+            AxiosMethod.using_auth = true
+            AxiosMethod.token = this.$cookies.get('adminToken')
+            AxiosMethod.end_point = `seller/sku/search?id=${search}`
+            let data = await AxiosMethod.axios_get()
+            if (data) {
+                this.skuSearchList = data.data.data
+            }
+        },
+
+        async assignSku(id, skuId) {
+            const formData = new FormData()
+            const AxiosMethod = new AxiosCall()
+            AxiosMethod.using_auth = true
+            AxiosMethod.store = this.$store
+            AxiosMethod.token = this.$cookies.get('adminToken')
+            AxiosMethod.end_point = `page/home/section/slider/${this.homeSinglePartitionSlider.slider_id}/sku/attach`
+            formData.append('shps', id)
+            formData.append('sku_id', skuId)
+            formData.append('priority', 1)
+            formData.append('partition_id', this.$route.params.partitionId)
+            AxiosMethod.form = formData
+            let data = await AxiosMethod.axios_post()
+            if (data) {
+                this.getHomeSinglePartitionSlider()
+                openToast(
+                    this.$store,
+                    'کد کالا با موفقیت افزوده شد.',
+                    "success"
+                );
+            }
+        }
+    },
+
+    mounted() {
+        this.getHomeSinglePartitionSlider()
+    },
+
+    watch: {
+        homeSection(val) {
+            this.title = val.label
+        },
+        dataSkuTableLength(val) {
+            this.addSkuPerPage(val)
+        },
+        confirmModal(val) {
+            if (localStorage.getItem('deleteObject') === 'done') {
+                if (!val) {
+                    this.getHomeSinglePartitionSlider();
+                    openToast(
+                        this.$store,
+                        'محصول با موفقیت حذف شد',
+                        "success"
+                    );
+                    localStorage.removeItem('deleteObject')
+                }
+            }
+        },
+        $route(to, from) {
+            this.getHomeSection()
+        }
+    }
 
 }
 </script>
--- src/components/HomePage/HomePageSpecialSalesList.vue
+++ src/components/HomePage/HomePageSpecialSalesList.vue
@@ -1,90 +1,85 @@
 <template>
-  <div class="h-100 d-flex flex-column align-stretch seller">
+<div class="h-100 d-flex flex-column align-stretch seller">
     <v-card height="70" class="ma-5 br-12 stretch-card-header-70">
-      <v-row
-          justify="center"
-          align="center"
-          class="px-10 py-4">
-        <v-col cols="6" >
-          <v-btn
-              @click="$router.push(`/home-page/${$route.params.sectionId}/special-sales/add`)"
-              color="primary500"
-              height="40"
-              rounded
-              class="px-8 mt-1">
-            <template v-slot:prepend>
-              <v-icon>mdi-plus</v-icon>
-            </template>
-            افزودن اسلایدر
-          </v-btn>
-        </v-col>
+        <v-row
+            justify="center"
+            align="center"
+            class="px-10 py-4">
+            <v-col cols="6">
+                <v-btn
+                    @click="$router.push(`/home-page/${$route.params.sectionId}/special-sales/add`)"
+                    color="primary500"
+                    height="40"
+                    rounded
+                    class="px-8 mt-1">
+                    <template v-slot:prepend>
+                        <v-icon>mdi-plus</v-icon>
+                    </template>
+                    افزودن اسلایدر
+                </v-btn>
+            </v-col>
 
-        <v-col cols="6">
-          <v-row justify="end" >
-            <ModalColumnFilter :changeHeaderShow="changeHeaderShow" :header="specialSalesHeader" />
-
-<!--            <ModalTableFilter path="supplier/index" :filterField="filterBannerField" />-->
-          </v-row>
-        </v-col>
-      </v-row>
+            <v-col cols="6">
+                <v-row justify="end">
+                    <ModalColumnFilter :changeHeaderShow="changeHeaderShow" :header="specialSalesHeader" />
+                </v-row>
+            </v-col>
+        </v-row>
     </v-card>
 
     <v-card class="ma-5 br-12 flex-grow-1 d-flex flex-column align-stretch" height="580">
-      <Table
-          class="flex-grow-1"
-          :header="specialSalesHeader"
-          :items="homeSection?.sliders"
-          editUrl="/seller/edit/"
-          activePath="seller/crud/update/activation/"
-          deletePath="page/home/section/slider/delete/"
-          changeStatusUrl="seller/crud/update/contract/"
-          :loading="loading"
+        <Table
+            class="flex-grow-1"
+            :header="specialSalesHeader"
+            :items="homeSection?.sliders"
+            editUrl="/seller/edit/"
+            activePath="seller/crud/update/activation/"
+            deletePath="page/home/section/slider/delete/"
+            changeStatusUrl="seller/crud/update/contract/"
+            :loading="loading"
+            updateUrl="seller/csv/mass-update"
+            model="sku" />
 
-          updateUrl="seller/csv/mass-update"
-          model="sku" />
+        <v-divider />
 
-      <v-divider />
+        <v-card-actions class="pb-3">
+            <v-row class="px-8">
+                <v-col cols="3" class="d-flex justify-start" />
 
-      <v-card-actions class="pb-3">
-        <v-row class="px-8">
-          <v-col cols="3" class="d-flex justify-start">
+                <v-col cols="6" class="d-flex justify-center">
+                    <div class="text-center">
+                        <v-pagination
+                            v-model="skuPage"
+                            :length="pageLength"
+                            rounded="circle"
+                            size="40"
+                            :total-visible="4"
+                            prev-icon="mdi-chevron-right"
+                            next-icon="mdi-chevron-left" />
+                    </div>
+                </v-col>
 
-          </v-col>
-
-          <v-col cols="6" class="d-flex justify-center">
-            <div class="text-center">
-              <v-pagination
-                  v-model="skuPage"
-                  :length="pageLength"
-                  rounded="circle"
-                  size="40"
-                  :total-visible="4"
-                  prev-icon="mdi-chevron-right"
-                  next-icon="mdi-chevron-left" />
-            </div>
-          </v-col>
-
-          <v-col cols="3" class="d-flex justify-end">
-            <div
-                align="center"
-                id="rowSection"
-                class="d-flex align-center">
+                <v-col cols="3" class="d-flex justify-end">
+                    <div
+                        align="center"
+                        id="rowSection"
+                        class="d-flex align-center">
                         <span class="ml-5">
                             تعداد سطر در هر صفحه
                         </span>
-              <span class="mt-2" id="row-selector">
+                        <span class="mt-2" id="row-selector">
                             <v-select
                                 v-model="dataTableLength"
                                 class="t1330"
                                 variant="outlined"
                                 :items="[25,50,100]" />
                         </span>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card-actions>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card-actions>
     </v-card>
-  </div>
+</div>
 </template>
 
 <script>
@@ -94,61 +89,76 @@ import ModalTableFilter from '@/components/Menu/Filter/Filter.vue'
 import ModalColumnFilter from '@/components/Public/ModalColumnFilter.vue'
 import ModalGroupAdd from '@/components/Public/ModalGroupAdd.vue'
 import ModalExcelDownload from "@/components/Public/ModalExcelDownload.vue";
-import { openToast} from "@/assets/js/functions";
+import {
+    openToast
+} from "@/assets/js/functions";
 import ModalAddBanner from "@/components/HomePage/Modals/ModalAddBanner.vue";
 export default {
-  setup(props) {
-    const {
-      specialSalesHeader , getHomeSection , homeSection ,  dataTableLength, page , filterBannerField , loading ,
-    } = new Home();
-    return {
-      specialSalesHeader , getHomeSection , homeSection ,  dataTableLength, page , filterBannerField , loading
-    };
-  },
-
-  components: {
-    ModalAddBanner,
-    Table,
-    ModalGroupAdd,
-    ModalTableFilter,
-    ModalColumnFilter,
-    ModalExcelDownload,
-  },
-
-  computed: {
-    confirmModal() {
-      return this.$store.getters['get_confirmForm'].confirmModal
-    }
-  },
-
-  methods: {
-    changeHeaderShow(index, value) {
-      this.bannerHeader[index].show = value
+    setup(props) {
+        const {
+            specialSalesHeader,
+            getHomeSection,
+            homeSection,
+            dataTableLength,
+            page,
+            filterBannerField,
+            loading,
+        } = new Home();
+        return {
+            specialSalesHeader,
+            getHomeSection,
+            homeSection,
+            dataTableLength,
+            page,
+            filterBannerField,
+            loading
+        };
     },
 
-  },
-
-  mounted() {
-    this.getHomeSection()
-  },
-
-  watch: {
-    dataTableLength(val) {
-      this.addPerPage(val)
+    components: {
+        ModalAddBanner,
+        Table,
+        ModalGroupAdd,
+        ModalTableFilter,
+        ModalColumnFilter,
+        ModalExcelDownload,
     },
-    confirmModal(val) {
-      if (localStorage.getItem('deleteObject')) {
-        if (!val) {
-          this.getHomeSection()
-          openToast(
-              this.$store,
-              'آیتم مورد نظر با موفقیت حذف شد',
-              "success"
-          );
-          localStorage.removeItem('deleteObject')
+
+    computed: {
+        confirmModal() {
+            return this.$store.getters['get_confirmForm'].confirmModal
         }
-      }
     },
-  }
+
+    methods: {
+        changeHeaderShow(index, value) {
+            this.bannerHeader[index].show = value
+        },
+
+    },
+
+    mounted() {
+        this.getHomeSection()
+    },
+
+    watch: {
+        dataTableLength(val) {
+            this.addPerPage(val)
+        },
+        
+        confirmModal(val) {
+            if (localStorage.getItem('deleteObject')) {
+                if (!val) {
+                    this.getHomeSection()
+                    openToast(
+                        this.$store,
+                        'آیتم مورد نظر با موفقیت حذف شد',
+                        "success"
+                    );
+                    localStorage.removeItem('deleteObject')
+                }
+            }
+        },
+    }
 }
 </script>
