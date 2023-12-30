858fa3357b518310fb8ea0865bf659a847bdf1eb
zohreh <zohreh.tt@gmail.com>
2023-12-30 16:57:10 +0330

    feat(order user edit): Add user edit page

--- src/views/Orders/orderEditUser.vue
+++ src/views/Orders/orderEditUser.vue
@@ -1,10 +1,15 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
-<script>
-export default {
-    
-}
+<script setup>
+import DashboardLayout from '@/components/Layouts/DashboardLayout.vue'
+import Header from '@/components/Public/Header.vue'
+import EditUser from '@/components/Orders/EditUser/EditUser.vue'
 </script>
+
+<template>
+  <v-layout class="bg-gray">
+    <DashboardLayout />
+    <v-main class="h-100vh">
+      <Header />
+      <EditUser />
+    </v-main>
+  </v-layout>
+</template>
