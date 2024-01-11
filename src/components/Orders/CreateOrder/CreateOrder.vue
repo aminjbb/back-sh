735d9f6fa656283f355ea7549620d7fccf17598e
amin jabari <aminjabari.dev@gmail.com>
2024-01-11 14:39:51 +0330

    feat(admin-order):fix order create with admin

---
+++ src/components/Orders/CreateOrder/CreateOrder.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="create-product flex-column d-flex vh-100">
+    <v-card class="ma-5 br-12 pb-15 flex-grow-1" height="600">
+      <CreateOrderForm ref="CreateOrderForm" />
+
+      <footer class="create-warehouse__actions">
+        <v-row justify="end" class="pl-10 pt-5">
+          <v-btn
+              :loading="loading"
+
+              color="primary500"
+              height="40"
+              rounded
+              class="px-8 mt-1">
+
+            تایید
+          </v-btn>
+
+        </v-row>
+      </footer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import CreateOrderForm from "@/components/Orders/CreateOrder/CreateOrderForm.vue";
+import {
+  AxiosCall
+} from "@/assets/js/axios_call";
+import {
+  convertDateToGregorian,
+  openToast
+} from "@/assets/js/functions";
+
+export default {
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  components: {
+    CreateOrderForm
+  },
+  methods: {
+    validate() {
+
+      const activeDay = this.$refs.WarehouseForm.days.filter(el => el.active)
+      const timeCondination = activeDay.find(el => el.endTime < el.startTime)
+      if (timeCondination){
+        openToast(this.$store , 'ساعت پایان نباید قبل ساعت شروع باشد' , 'error')
+      }
+      else{
+        this.$refs.WarehouseForm.$refs.addWarehouse.validate()
+        setTimeout(() => {
+          if (this.$refs.WarehouseForm.valid)this.createWarehouse()
+        }, 200)
+      }
+
+    },
+
+    async createWarehouse() {
+      this.loading = true
+      let formData = new FormData();
+      const AxiosMethod = new AxiosCall()
+      AxiosMethod.end_point = 'warehouse/crud/create'
+      formData.append('name', this.$refs.WarehouseForm.form.name)
+      this.$refs.WarehouseForm.form.type.forEach((type, index) => {
+        formData.append(`types[${index}]`, type)
+      })
+      formData.append('market_storage_count', this.$refs.WarehouseForm.form.marketCapacity)
+      formData.append('retail_storage_count', this.$refs.WarehouseForm.form.RetailCapacity)
+      formData.append('address', this.$refs.WarehouseForm.form.address)
+      formData.append('postal_code', this.$refs.WarehouseForm.form.postalCode)
+      formData.append('phone_number', this.$refs.WarehouseForm.form.phoneNumber)
+      formData.append('lat', this.$refs.WarehouseForm.form.latLong.latitude)
+      formData.append('long', this.$refs.WarehouseForm.form.latLong.longitude)
+      formData.append('is_active', 0)
+      AxiosMethod.form = formData
+      AxiosMethod.store = this.$store
+      AxiosMethod.using_auth = true
+      AxiosMethod.token = this.$cookies.get('adminToken')
+      let data = await AxiosMethod.axios_post()
+      if (data) {
+
+        this.createWorkDayWarehouse(data.data.id)
+        openToast(this.$store,
+            'انبار با موفقیت ایحاد شد.',
+            "success")
+      } else {
+        this.loading = false
+        openToast(this.$store,
+            'ایحاد انبار با مشکل مواجه شد',
+            "error")
+      }
+    },
+
+
+  },
+  computed: {
+    avatar() {
+      return this.$store.getters['get_avatar']
+    }
+  },
+}
+</script>
