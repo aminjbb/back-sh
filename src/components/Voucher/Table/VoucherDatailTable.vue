e08a8ec70b26e4cf07e6b4645a0eafcfcae7e9e7
amin jabari <aminjabari.dev@gmail.com>
2024-01-15 14:12:27 +0330

    feat(voucher):fix create voucher

---
+++ src/components/Voucher/Table/VoucherDatailTable.vue
@@ -0,0 +1,236 @@
+<template>
+  <div :style="{height: height}" class="c-table pl-1">
+    <v-progress-linear
+        color="primary500"
+        indeterminate
+        reverse
+        v-if="loading" />
+
+    <header class="c-table__header d-flex justify-between">
+      <template v-for="(head, index) in header">
+        <div
+            v-if="head.show"
+            @click="createOrdering(head.value, head.order)"
+            class="text-center c-table__header__item t12500"
+            :class="head.order == true ? 'pointer' : ''"
+            :key="index"
+            :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+          <v-icon v-if="head.order == true"  />
+          {{head.name}}
+        </div>
+
+      </template>
+
+    </header>
+
+    <div class="stretch-table">
+      <div v-if="items && items.length > 0 && !loading" class="c-table__contents">
+        <div
+            v-for="(item , index) in items"
+            :key="index"
+            :class="oddIndex(index) ? 'bg-gray90' : ''"
+            class="d-flex justify-between c-table__contents__row">
+          <div
+              v-if="header[0].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span class="t14300 text-gray500 py-5 number-font">
+                        {{rowIndexTable(index)}}
+                    </span>
+          </div>
+
+          <div
+              v-if="item.first_name && header[1].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span class="t14300 text-gray500 py-5">
+                        {{ item.first_name }}
+                    </span>
+          </div>
+
+          <div
+              v-if="item.last_name && header[2].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span class="t14300 text-gray500 py-5">
+                        {{ item.last_name }}
+                    </span>
+          </div>
+
+          <div
+              v-if="item.phone_number && header[3].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span class="t14300 text-gray500 py-5 number-font">
+                        {{ item.phone_number }}
+                    </span>
+          </div>
+          <div
+              v-if=" header[4].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span v-if="item.role" class="t14300 text-gray500 py-5 number-font">
+                        {{ item.role?.label }}
+                    </span>
+            <span v-else>----</span>
+          </div>
+          <div
+              v-if=" header[5].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span class="t14300 text-gray500 py-5 number-font">
+                        {{ convertDateToJalai(item.created_at , '-' , true) }}
+                    </span>
+          </div>
+          <div
+              v-if=" header[6].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span v-if="item.last_logged_in" class="t14300 text-gray500 py-5 number-font">
+                        {{ convertDateToJalai(item.last_logged_in , '-' , true) }}
+                    </span>
+            <span v-else class="t14300 text-gray500 py-5 number-font">
+                        ---
+                    </span>
+          </div>
+
+          <div
+              v-if=" header[7].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+                    <span v-if="item.email" class="t14300 text-gray500 py-5">
+                        {{ item.email }}
+                    </span>
+            <span v-else class="t14300 text-gray500 py-5">
+                        ----
+                    </span>
+          </div>
+          <div
+              v-if=" header[7].show"
+              class="c-table__contents__item justify-center"
+              :style="{ width: itemsWidth, flex: `0 0 ${itemsWidth}` }">
+            <v-switch
+                v-model="isBan[index]"
+                inset
+                color="success"
+                @change="changeBan(index,item)" />
+          </div>
+
+        </div>
+      </div>
+      <div v-else class="null-data-table d-flex justify-center align-center flex-column">
+        <img src="@/assets/img/NullTable.png" alt="shavaz image">
+        <div class="d-flex justify-center align-center flex-column">
+          <span class="title4 text-black mb-5">لیست خالی!</span>
+          <span class="t14300 text-gray500">تاکنون داده‌ای به این صفحه، افزوده نشده است.</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  isOdd
+} from '@/assets/js/functions'
+
+import {
+  PanelFilter
+} from "@/assets/js/filter"
+export default {
+
+  props: {
+    /**
+     * List Items for header
+     */
+    header: [],
+
+    /**
+     * List of items
+     */
+    items: [],
+
+    /**
+     * Model
+     */
+    model: '',
+
+    /**
+     * Height
+     */
+    height: {
+      type: String,
+      default: '500',
+    },
+
+    /**
+     * Page on table
+     */
+    page: {
+      type: Number,
+      default: 1
+    },
+
+    /**
+     * PerPage of data
+     */
+    perPage: {
+      type: Number,
+      default: 1
+    },
+
+    /**
+     * Active loading
+     */
+    loading: {
+      type: Boolean,
+      default: false
+    },
+
+
+  },
+
+  data() {
+    return {
+      order_type: "desc",
+      ordering: {},
+      per_page: '25',
+      filter: [],
+      isBan:[],
+      panelFilter: new PanelFilter(),
+    }
+  },
+
+  computed: {
+    /**
+     * Get each items table based of header length
+     */
+    itemsWidth() {
+      let headerLength = 0;
+      if (this.header && this.header.length > 0) {
+        this.header.forEach(element => {
+          if (element.show == true) {
+            headerLength++;
+          }
+        });
+        const width = 100 / (headerLength + 1);
+        return `${width}%`;
+      }
+      return 'auto';
+    },
+  },
+
+  methods: {
+
+    /**
+     * Return odd index
+     * @param {*} index
+     */
+    oddIndex(index) {
+      return isOdd(index)
+    },
+
+  },
+
+}
+</script>
