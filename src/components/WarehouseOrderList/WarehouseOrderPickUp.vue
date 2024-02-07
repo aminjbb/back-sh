aadaf66bb1f4581a0cf991338491af7b754170ee
amin jabari <aminjabari.dev@gmail.com>
2024-02-07 14:38:42 +0330

    fix(processing shipmet):add if to table model == upcoming

--- src/components/WarehouseOrderList/WarehouseOrderPickUp.vue
+++ src/components/WarehouseOrderList/WarehouseOrderPickUp.vue
@@ -20,44 +20,44 @@
           </v-icon>
         </div>
       </div>
-      <v-card class="ma-5 br-15" max-height="116">
+      <v-card class="mx-5 mb-2 br-15" max-height="116">
         <div class="d-flex justify-center my-3">
-          <span class="t20400">
+          <span class="t12400">
             جایگاه کالا
           </span>
         </div>
         <v-divider/>
-        <div class="text-right my-5 px-5 d-flex justify-space-between px-10">
+        <div class="text-right my-2 px-5 d-flex justify-space-between px-10">
           <div>
-           <span class="t16400">
+           <span class="t12400">
            شماره ردیف :
-            <span class="text-gray600 number-font">
+            <span class="t12400 text-gray600 number-font">
               {{ pickUpShps?.placement?.row_numbe }}
             </span>
           </span>
           </div>
           <div>
-          <span class="t16400">
+          <span class="t12400">
            شماره قفسه  :
-            <span class="text-gray600 number-font">
+            <span class=" t12400 text-gray600 number-font">
               {{ pickUpShps?.placement?.placement_number }}
             </span>
           </span>
           </div>
         </div>
-        <div class="text-right mt-10 px-5 d-flex justify-space-between px-10">
+        <div class="text-right  px-5 d-flex justify-space-between px-10">
           <div>
-           <span class="t16400">
+           <span class="t12400">
            شماره طبقه :
-            <span class="text-gray600 number-font">
+            <span class=" t12400 text-gray600 number-font">
                {{ pickUpShps?.placement?.step_number }}
             </span>
           </span>
           </div>
           <div>
-          <span class="t16400">
+          <span class="t12400">
            شماره شلف   :
-            <span class="text-gray600 number-font">
+            <span class=" t12400 text-gray600 number-font">
              {{ pickUpShps?.placement?.shelf_number }}
             </span>
           </span>
@@ -74,15 +74,15 @@
               <v-card class="ml-5 br br__12 d-flex justify-center align-center" height="52" width="52"
                       color="primary500">
             <span class="text-white number-font">
-             {{ pickUpShps?.count }}
+             {{ pickUpCount }}
             </span>
               </v-card>
               <span class="t16400 text-black">عدد مانده</span>
             </v-card>
             <v-card class="mt-2 py-5">
               <div class="d-flex justify-center">
-                <img src="@/assets/img/productImge.png" width="150" height="150" alt="">
-                <!--              <img :src="pickUpShps?.shps?.sku?.image_url" width="150" height="150" alt="">-->
+<!--                <img src="@/assets/img/productImge.png" width="150" height="150" alt="">-->
+                              <img :src="pickUpShps?.shps?.sku?.image_url" width="100" height="100" alt="">
               </div>
               <div class="text-center px-10 my-3">
               <span class="text-gray600">
@@ -110,7 +110,7 @@
             rounded
             @click="$router.push('/locating/dashboard')"
             class="px-8 mt-5">
-          بازگشت به داشبورد
+            بازگشت به داشبورد
         </v-btn>
       </div>
     </div>
@@ -152,9 +152,9 @@ import HandheldDrawer from "@/components/Layouts/HandheldDrawer.vue";
 
 export default {
   setup() {
-    const {getPickUpShps, pickUpShps, pickUpIsNull , loading} = new WarehouseOrder()
+    const {getPickUpShps, pickUpShps, pickUpIsNull , loading , pickUpCount} = new WarehouseOrder()
     return {
-      getPickUpShps, pickUpShps, pickUpIsNull , loading
+      getPickUpShps, pickUpShps, pickUpIsNull , loading , pickUpCount
     }
   },
   data() {
@@ -163,34 +163,40 @@ export default {
       qrCode: '',
       shpssBarCode: '',
       placeCount: 0,
-      pickUpDone: false
+      pickUpDone: false,
+
     }
   },
 
   mounted() {
     this.getPickUpShps()
-    var element = document.body // You must specify element here.
-    element.addEventListener('keydown', e => {
-      if (e.key == 'Enter') this.scanQrCode()
-      else this.qrCode += e.key
-    });
   },
-
   methods: {
     scanQrCode() {
       this.pickUpshpss(this.shpssBarCode)
     },
     async pickUpshpss(barcode) {
-      const AxiosMethod = new AxiosCall()
-      const formData = new FormData()
-      formData.append('barcode', barcode)
-      formData.append('placement_id', this.pickUpShps?.placement?.id)
-      AxiosMethod.using_auth = true
-      AxiosMethod.token = cookies.cookies.get('adminToken')
-      AxiosMethod.end_point = 'admin/order/pick'
-      let data = await AxiosMethod.axios_post()
-      if (data) {
-        this.getPickUpShps()
+      try {
+        this.loading = true
+        const AxiosMethod = new AxiosCall()
+        const formData = new FormData()
+        formData.append('barcode', barcode)
+        formData.append('placement_id', this.pickUpShps?.placement_id)
+        AxiosMethod.using_auth = true
+        AxiosMethod.token = cookies.cookies.get('adminToken')
+        AxiosMethod.end_point = 'admin/order/pick'
+        let data = await AxiosMethod.axios_post()
+        if (data) {
+          this.loading = false
+
+          this.getPickUpShps()
+        }
+        else {
+          this.loading = false
+        }
+      }
+      catch (e) {
+        this.loading = false
       }
     },
   },
