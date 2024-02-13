226d5e0d74b62eeb55a28faeec4ccfc4e84b7f09
Hossein Nasiri <imhsnnasiri@gmail.com>
2024-02-13 15:43:25 +0330

    T

--- src/components/ProcessingShipment/ProcessingShipmentPrintBarCode.vue
+++ src/components/ProcessingShipment/ProcessingShipmentPrintBarCode.vue
@@ -1,23 +1,14 @@
 <template>
-  <div class="text-center d--ltr" id="printableArea-shipmentBarcode">
-    <v-row class="pa-0 ma-0">
-      <v-col v-for="(barCode , index) in detail" cols="6" :key="'shpss-list-barcode'+index" class="pa-0 " :class="index < (detail.length - 1) ? 'mb-11' : 'mb-0'">
-        <!--        <div class="d-flex justify-space-between ">-->
-        <!--          <span>{{barCode?.shps?.seller?.id}}</span>-->
-        <!--          <span>{{barCode?.shps?.seller?.shopping_name}}</span>-->
-        <!--        </div>-->
-        <!--        <div class="text-center">-->
-        <!--          <img width="180" height="70" :src="basUrl + barCode.shps?.barcode_image">-->
-        <!--        </div>-->
-        <!--"-->
-        <div class="text-center" style="font-size: 8px ;direction: rtl">
+  <div class="barcode-label-container" id="printableArea-shipmentBarcode">
+    <v-row v-for="(barCode, index) in detail" :key="'shpss-list-barcode'+index" class="barcode-label">
+      <v-col cols="12">
+        <div class="barcode-sku">
           {{ barCode?.shps?.sku?.label }}
         </div>
-        <div class="text-center">
-          <img width="100" height="45" :src="basUrl + barCode?.barcode_image" :alt="barCode?.barcode">
+        <div class="barcode-image">
+          <img :src="baseUrl + barCode?.barcode_image" :alt="barCode?.barcode">
         </div>
-        <!--        style="font-size: 7px"-->
-        <div class="text-center" style="font-size: 8px ;direction: rtl">
+        <div class="barcode-value">
           {{ barCode?.barcode }}
         </div>
       </v-col>
@@ -62,10 +53,31 @@ export default {
   },
 
   computed: {
-    basUrl() {
+    baseUrl() {
       return 'https://api.shvz.ir/'
     },
 
   }
 }
 </script>
+<style scoped>
+.barcode-label-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.barcode-label {
+  height: 140mm;
+  width: 35mm;
+  margin-bottom: 10mm;
+}
+
+.barcode-sku, .barcode-image, .barcode-value {
+  text-align: center;
+}
+
+.barcode-image > img {
+  width: 100%;
+  height: auto;
+}
+</style>
