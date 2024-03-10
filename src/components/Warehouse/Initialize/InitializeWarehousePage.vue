c75d2f7675461e1c220d2c9f4a01b22f6b68d1ed
Hossein Nasiri <imhsnnasiri@gmail.com>
2024-03-10 22:10:09 +0330

    Fix Slug Instead of Id

---
+++ src/components/Warehouse/Initialize/InitializeWarehousePage.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card height="100vh">
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <HandheldDrawer/>
+        <span class="t20400">
+          انبارگردانی
+        </span>
+      </v-col>
+    </v-row>
+    <v-row class="mx-2">
+      <v-col cols="12">
+        شلف
+        <v-text-field
+            hint="شلف را اسکن کنید"
+            v-model="shelfBarcode"
+        />
+      </v-col>
+      Sku
+      <v-col cols="12">
+        <v-text-field
+            hint="sku را اسکن کنید"
+            v-model="skuBarcode"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+            type="number"
+            hint="تعداد"
+            v-model="count"
+        />
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <v-btn width="150" @click="submit" color="success">تایید</v-btn>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+
+import HandheldDrawer from "@/components/Layouts/HandheldDrawer.vue";
+
+export default {
+  components: {HandheldDrawer},
+  data() {
+    return {
+      shelfBarcode: null,
+      skuBarcode: null,
+      count: 0,
+    }
+  },
+  methods:{
+    submit(){
+
+    }
+  }
+}
+</script>
+<style>
+.pick-up__counter {
+  position: absolute;
+  top: 36px;
+}
+</style>
---
+++ src/views/Warehouse/Initialize/InitilizeWarehouse.vue
@@ -0,0 +1,16 @@
+<script setup>
+import InitializeWarehousePage from '@/components/Warehouse/Initialize/InitializeWarehousePage.vue'
+</script>
+
+<template>
+  <v-layout class="bg-gray">
+    <v-main class="justify-center">
+      <div class="scan-container">
+        <InitializeWarehousePage />
+      </div>
+    </v-main>
+  </v-layout>
+</template>
+<style>
+
+</style>
