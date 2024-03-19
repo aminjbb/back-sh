6af3b3eaefc8b1501b394a13287aa092b8475170
amin jabari <aminjabari.dev@gmail.com>
2024-03-19 17:26:33 +0330

    feat(relocating):add api

--- src/components/Layouts/HandheldDrawer.vue
+++ src/components/Layouts/HandheldDrawer.vue
@@ -7,13 +7,14 @@
 
     <v-list density="compact" nav>
       <v-list-item prepend-icon="mdi-view-dashboard-outline" href="/locating/dashboard" title="داشبورد"  value="home"></v-list-item>
-      <v-list-item prepend-icon="mdi-archive-outline" href="/locating/index" title="جایگذاری بسته" value="about"></v-list-item>
-      <v-list-item prepend-icon="mdi-bookshelf" href="/locating/package/shps-list" title="جایگذاری محصولات" value="about"></v-list-item>
-      <v-list-item prepend-icon="mdi-bookshelf" href="/locating/singular-locating" title="جایگداری تکی" value="about"></v-list-item>
-      <v-list-item prepend-icon="mdi-shopping-outline" href="/warehouse-orders/pick-up" title="جمع آوری کالا" value="about"></v-list-item>
-      <v-list-item prepend-icon="mdi-package-variant-closed"  href="/warehouse-orders/sorting" title="جایگذاری در جایگاه سورتینگ" value="about"></v-list-item>
-      <v-list-item prepend-icon="mdi-package-variant-closed-minus"  href="/warehouse-orders/un-load" title="تخلیه جایگاه سورتینگ" value="about"></v-list-item>
-      <v-list-item @click="logOut()" prepend-icon="mdi-exit-to-app" title="خروج از حساب کاربری" value="about"></v-list-item>
+      <v-list-item prepend-icon="mdi-elevator-down" href="/locating/initilize" title="جایگذاری اولیه"  value="initilize"></v-list-item>
+      <v-list-item prepend-icon="mdi-archive-outline" href="/locating/index" title="جایگذاری بسته" value="locating"></v-list-item>
+      <v-list-item prepend-icon="mdi-bookshelf" href="/locating/package/shps-list" title="جایگذاری محصولات" value="package"></v-list-item>
+      <v-list-item prepend-icon="mdi-bookshelf" href="/locating/singular-locating" title="جایگداری تکی" value="singular-locating"></v-list-item>
+      <v-list-item prepend-icon="mdi-shopping-outline" href="/warehouse-orders/pick-up" title="جمع آوری کالا" value="pick-up"></v-list-item>
+      <v-list-item prepend-icon="mdi-package-variant-closed"  href="/warehouse-orders/sorting" title="جایگذاری در جایگاه سورتینگ" value="sorting"></v-list-item>
+      <v-list-item prepend-icon="mdi-package-variant-closed-minus"  href="/warehouse-orders/un-load" title="تخلیه جایگاه سورتینگ" value="un-load"></v-list-item>
+      <v-list-item @click="logOut()" prepend-icon="mdi-exit-to-app" title="خروج از حساب کاربری" value="logOut"></v-list-item>
     </v-list>
   </v-navigation-drawer>
   <div class="d-flex justify-center align-center h-100">
