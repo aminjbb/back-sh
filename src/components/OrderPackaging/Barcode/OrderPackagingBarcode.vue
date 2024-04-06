8256c682c7e7f0f8708ab92b21fee8d8c7c322e5
amin jabari <aminjabari.dev@gmail.com>
2024-04-06 16:20:55 +0330

    T

---
+++ src/components/OrderPackaging/Barcode/OrderPackagingBarcode.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="barcode-container">
+    <svg id="barcode"></svg>
+  </div>
+</template>
+
+<script>
+import JsBarcode from "jsbarcode";
+export default {
+  props: {
+    barcodeValue: [Number, String],
+    format: String,
+    index: Number,
+    text: String,
+    order:String
+  },
+  data() {
+    return {
+      fontSize: 12,
+    }
+  },
+  mounted() {
+    let settings = {
+      format: this.format,
+      textAlign: 'rif',
+      textPosition: 'top',
+      fontOptions: 'bold',
+      fontSize: 18,
+      textMargin: 8,
+      height: 45,
+    };
+    JsBarcode("#barcode", this.barcodeValue, settings);
+  }
+
+}
+</script>
+
+<style scoped>
+svg {
+  width: 160px !important;
+  height: 65px!important;
+}
+
+.barcode-container {
+  position: relative;
+}
+
+.barcode-text {
+  position: absolute;
+  top: 65px; /* Adjust this value according to your needs. */
+  width: 100%;
+  text-align: right;
+}
+</style>
