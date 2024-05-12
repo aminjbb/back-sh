35ad51b85c770571a96a93dd3e42e06aa3ca4dd2
amin jabari <aminjabari.dev@gmail.com>
2024-05-12 13:30:58 +0330

    feat(stocktaking):add stocktaking in handheld

---
+++ src/views/Warehouse/PrintLabelView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DashboardLayout from '@/components/Layouts/DashboardLayout.vue'
+import Header from '@/components/Public/Header.vue'
+import PrintLabel from '@/components/Warehouse/PrintLabel.vue'
+</script>
+
+<template>
+  <v-layout class="bg-gray">
+    <DashboardLayout />
+    <v-main class="h-100vh">
+      <Header />
+      <PrintLabel />
+    </v-main>
+  </v-layout>
+</template>
