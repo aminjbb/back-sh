7fee011aadc5bf23310ea204a74092ea6a24df61
Hamid Baseri <hamid.baseri@gmail.com>
2024-06-09 11:47:40 +0330

    minor improves

--- src/components/Reports/ReportCard.vue
+++ src/components/Reports/ReportCard.vue
@@ -2,10 +2,10 @@
   <v-col class="py-0 px-0" cols="12" sm="4">
     <v-card class="ma-2 px-2 py-2 flex-grow-1 rounded-lg">
       <v-row justify="space-between" align="center">
-        <v-col v-if="itemImagePath" cols="3">
+        <v-col v-if="itemImagePath" cols="2">
           <img class="w-100" :src=itemImagePath :width="100" alt="order-box">
         </v-col>
-        <v-col cols="md-9 sm-12">
+        <v-col cols="md-10 sm-12">
           <v-skeleton-loader type="list-item-two-line" v-if="loading"></v-skeleton-loader>
           <template v-else>
             <v-row class="my-1">
@@ -21,7 +21,7 @@
             </span>
             </v-row>
             <v-row class="mb-1">
-              <span class="number-font">{{ itemValue?.toLocaleString() }}</span>
+              <span class="number-font">{{ itemValue?.toLocaleString() ?? '&nbsp;'}}</span>
             </v-row>
           </template>
 
