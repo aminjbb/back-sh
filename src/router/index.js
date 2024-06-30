9ecb1efd9518873141ccf7fd0d8e2ffc193dbb58
Shavaz <shavaz@Shavazs-MacBook-Air.local>
2024-06-30 17:06:15 +0330

    feat(order tracking): Add order tracking page

--- src/router/index.js
+++ src/router/index.js
@@ -298,7 +298,8 @@ import ReplyCommentView from '../views/CommentAccepted/ReplyCommentView.vue'
 import ReportBugTaskView from '../views/ReportBugTask/ReportBugTaskView.vue'
 import CancelOrderView from "@/views/Orders/CancelOrderView.vue";
 
-
+/* Order Tracking */
+import OrderTrackingView from '../views/OrderTracking/OrderTrackingView.vue'
 
 
 
@@ -1883,6 +1884,22 @@ const router = createRouter({
                         name: 'بسته بندی سفارش ها'
                     }
                 },
+            ],
+        },
+        {
+            path: '/order-tracking', // order tracking
+            meta: {
+                name: 'پشیبانی'
+            },
+            children: [
+                {
+                    path: 'index',
+                    name: 'dorder tracking',
+                    component: OrderTrackingView,
+                    meta: {
+                        name: 'پیگیری سفارش مشتری'
+                    }
+                }
             ],
         },
         {
