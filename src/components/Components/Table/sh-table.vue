ef63615106eea86e1717a1816a2fbb8a2077ef33
unknown <habibi.idin@gmail.com>
2024-07-03 16:27:48 +0330

    feat(sh-table)

--- src/components/Components/Table/sh-table.vue
+++ src/components/Components/Table/sh-table.vue
@@ -1,99 +1,140 @@
 <template>
-  <v-data-table
-      :headers="header"
-      :items="items"
-      :items-per-page="perPage"
-      :page.sync="currentPage"
-      :loading="loading"
-      @update:options="updateOptions"
-      item-value="index">
-    <template v-slot:item.row="{ item, index }">
-      <span> {{rowIndexTable(index)}} </span>
-    </template>
+  <div class="c-table rounded-t-lg">
+    <v-data-table
+        class="table mt-5"
+        :headers="headers"
+        :items="items"
+        :show-select="isSelect"
+        hide-default-footer
 
-    <template v-slot:item.id="{ item }">
-      <span> {{ item.id }}</span>
-    </template>
-    <template v-slot:item.name="{ item }">
-      <div class="">
-          <span>
-            <img src="@/assets/img/shps_img.png" :alt="item.name">
-          </span>
-        <span> {{item.name}} </span>
-      </div>
-    </template>
-  </v-data-table>
+        item-value="index">
+      <template v-slot:item.row="{ item, index }">
+        <span class="number-font"> {{rowIndexTable(index)}} </span>
+      </template>
+      <template v-slot:item.id="{ item }">
+        <span class="number-font"> {{ item.id }}</span>
+      </template>
+      <template v-slot:item.created_at_fa="{ item }">
+        <span class="number-font">
+          {{ item.created_at_fa}}
+        </span>
+      </template>
+      <template v-slot:item.updated_at_fa="{ item }">
+        <span class="number-font">
+          {{ item.updated_at_fa}}
+        </span>
+      </template>
+      <template v-slot:item.identification_code="{ item }">
+        <span class="number-font">
+          {{ item.identification_code}}
+        </span>
+      </template>
+      <template v-slot:item.phone_number="{ item }">
+        <span class="number-font">
+          {{ item.phone_number}}
+        </span>
+      </template>
+      <template v-slot:item.action_driver="{ item }">
+        <v-menu :close-on-content-click="false" :location="location">
+          <template v-slot:activator="{ props }">
+            <v-icon v-bind="props">
+              mdi-dots-vertical
+            </v-icon>
+          </template>
+          <v-list class="c-table__more-options">
+            <v-list-item >
+              <v-list-item-title>
+                <div
+                    class="ma-5 pointer"
+                    @click="$router.push(`/driver-management/update/${item.id}`)">
+                  <v-icon size="small" class="text-grey-darken-1">mdi-pen</v-icon>
+                  <span class="mr-2 text-grey-darken-1 t14300">
+                                                ویرایش
+                                                </span>
+                </div>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>
+                <div class="ma-5 pointer" @click="removeItem(item.id)">
+                  <v-icon size="small" class="text-grey-darken-1">mdi-trash-can-outline</v-icon>
+                  <span class="mr-2 text-grey-darken-1 t14300">
+                                                حذف
+                                            </span>
+                </div>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+      <template v-slot:no-data>
+        <div class="d-flex justify-center align-center flex-column py-14">
+          <img src="@/assets/img/NullTable.png" alt="shavaz image">
+          <div class="d-flex justify-center align-center flex-column">
+            <span class="text-gray500 text-center text-body-1">اطلاعاتی برای نمایش وجود ندارد.</span>
+          </div>
+        </div>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
+import {
+  openConfirm,
+  isOdd
+} from "@/assets/js/functions";
 export default {
-  name: 'ReusableTable',
+  name: "Table",
+
   props: {
-    getDriverList: {
-      type: Function,
-      required: true
-    },
-    header: {
+    headers: {
       type: Array,
-      required: true
-    },
-    items: {
-      type: Array,
-      required: true
-    },
-    page: {
-      type: Number,
-      default: 1
-    },
-    perPage: {
-      type: Number,
-      default: 10
-    },
-    loading: {
-      type: Boolean,
-      default: false
-    },
-    activePath: {
-      type: String,
-      default: ''
-    },
-    updateUrl: {
-      type: String,
-      default: ''
-    },
+      default: () => []
+    },    items: Array,
+    isSelect: Boolean,
+    page: Number,
+    perPage:Number,
     deletePath: {
       type: String,
       default: ''
     },
-    model: {
-      type: String,
-      required: true
-    }
   },
+
+  components: {
+
+  },
+
   data() {
     return {
-      currentPage: this.page,
-    }
-  },
-  methods: {
-    updateOptions(options) {
-      this.$emit('update:options', options);
 
-    },
-    activateItem(item) {
-      console.log(`Activating item at ${this.activePath}${item.id}`);
-    },
-    deleteItem(item) {
-      console.log(`Deleting item at ${this.deletePath}${item.id}`);
-    },
-    massUpdate() {
-      console.log(`Mass updating at ${this.updateUrl}`);
     }
   },
-  watch: {
-    currentPage(newPage) {
-      this.$emit('update:page', newPage);
-    }
-  }
+
+  methods: {
+    rowIndexTable(index) {
+      let rowIndex = 0
+      if (this.page === 1) {
+        rowIndex = (1 + index)
+        return rowIndex
+      } else {
+        rowIndex = ((this.page - 1) * this.perPage) + index + 1
+        return rowIndex
+      }
+    },
+    removeItem(id) {
+      openConfirm(this.$store, "با حذف راننده دیگر به جزئیات آن دسترسی نخواهید داشت.آیا از انجام این کار اطمینان دارید؟", "حذف راننده","delete", this.deletePath + id, true)
+    },
+    oddIndex(index) {
+      return isOdd(index)
+    },
+
+
+  },
+  computed: {
+
+  },
 }
 </script>
+
--- src/components/DriverManagement/DriverManagementList.vue
+++ src/components/DriverManagement/DriverManagementList.vue
@@ -20,7 +20,7 @@
 
                  />
             </v-col>
-   
+
                <v-col cols="6">
                    <v-row justify="end">
                      <PanelFilter
@@ -32,12 +32,12 @@
                </v-col>
            </v-row>
        </v-card>
-   
+
        <v-card class="ma-5 mt-0 br-12 flex-grow-1 d-flex flex-column align-stretch" height="580">
            <ShTable
 
                class="flex-grow-1"
-               :header="headers"
+               :headers="headerDriver"
                :items="DriverManagementList.data"
                :page="page"
                :perPage="dataTableLength"
@@ -46,28 +46,15 @@
                updateUrl="page/csv/mass-update"
                deletePath="driver/crud/delete/"
                model="page" > </ShTable>
-<!--         <div>-->
-<!--&lt;!&ndash;           <ShTable&ndash;&gt;-->
-<!--&lt;!&ndash;               :headers="headers"&ndash;&gt;-->
-<!--&lt;!&ndash;               :items="plants"&ndash;&gt;-->
-<!--&lt;!&ndash;               :loading="loading"&ndash;&gt;-->
-<!--&lt;!&ndash;               :items-per-page="10"&ndash;&gt;-->
-<!--&lt;!&ndash;               :height="300"&ndash;&gt;-->
-<!--&lt;!&ndash;           >&ndash;&gt;-->
 
-<!--             <template v-slot:item.actions="{ item }">-->
-<!--               <v-btn @click="editItem(item)">Edit</v-btn>-->
-<!--             </template>-->
-<!--          -->
-<!--         </div>-->
            <v-divider />
-          {{header}}
+
            <v-card-actions class="pb-3">
                <v-row class="px-8">
                    <v-col cols="3" class="d-flex justify-start">
                        <ModalExcelDownload getEndPoint="cargo/csv/requested/cargo/export" />
                    </v-col>
-   
+
                    <v-col cols="6" class="d-flex justify-center">
                        <div class="text-center">
                            <v-pagination
@@ -80,7 +67,7 @@
                                next-icon="mdi-chevron-left" />
                        </div>
                    </v-col>
-   
+
                    <v-col cols="3" class="d-flex justify-end">
                        <div
                            align="center"
@@ -103,7 +90,7 @@
        </v-card>
    </div>
    </template>
-   
+
    <script>
    import Table from '@/components/DriverManagement/Table/Table.vue'
    import DriverManagement from "@/composables/DriverManagement";
@@ -120,85 +107,7 @@
      data() {
        return {
          perPageFilter:false,
-         headers: [
-           { title: 'Plant', align: 'start', sortable: false, key: 'name' },
-           { title: 'Light', align: 'end', key: 'light' },
-           { title: 'Height', align: 'end', key: 'height' },
-           { title: 'Pet Friendly', align: 'end', key: 'petFriendly' },
-           { title: 'Price ($)', align: 'end', key: 'price' },
-         ],
-         plants: [
-           {
-             name: 'Fern',
-             light: 'Low',
-             height: '20cm',
-             petFriendly: 'Yes',
-             price: 20,
-           },
-           {
-             name: 'Snake Plant',
-             light: 'Low',
-             height: '50cm',
-             petFriendly: 'No',
-             price: 35,
-           },
-           {
-             name: 'Monstera',
-             light: 'Medium',
-             height: '60cm',
-             petFriendly: 'No',
-             price: 50,
-           },
-           {
-             name: 'Pothos',
-             light: 'Low to medium',
-             height: '40cm',
-             petFriendly: 'Yes',
-             price: 25,
-           },
-           {
-             name: 'ZZ Plant',
-             light: 'Low to medium',
-             height: '90cm',
-             petFriendly: 'Yes',
-             price: 30,
-           },
-           {
-             name: 'Spider Plant',
-             light: 'Bright, indirect',
-             height: '30cm',
-             petFriendly: 'Yes',
-             price: 15,
-           },
-           {
-             name: 'Air Plant',
-             light: 'Bright, indirect',
-             height: '15cm',
-             petFriendly: 'Yes',
-             price: 10,
-           },
-           {
-             name: 'Peperomia',
-             light: 'Bright, indirect',
-             height: '25cm',
-             petFriendly: 'Yes',
-             price: 20,
-           },
-           {
-             name: 'Aloe Vera',
-             light: 'Bright, direct',
-             height: '30cm',
-             petFriendly: 'Yes',
-             price: 15,
-           },
-           {
-             name: 'Jade Plant',
-             light: 'Bright, direct',
-             height: '40cm',
-             petFriendly: 'Yes',
-             price: 25,
-           },
-         ],
+
          loading: false
        }
      },
@@ -210,10 +119,12 @@
                filterField,
                dataTableLength,
                page,
-               header,
+             headerDriver,
                addPagination,
                addPerPage,
-               loading
+               loading,
+             consignmentPrintList,
+             headerConsignmentShipmentList
            } = DriverManagement();
            return {
                pageLength,
@@ -222,13 +133,15 @@
                filterField,
                dataTableLength,
                page,
-               header,
+             headerDriver,
                addPagination,
                addPerPage,
-               loading
+               loading,
+             consignmentPrintList,
+             headerConsignmentShipmentList
            };
        },
-   
+
        components: {
          shBtn,
          PanelFilter,
@@ -236,16 +149,17 @@
            ModalGroupAdd,
            ModalColumnFilter,
            ModalExcelDownload,
-         ShTable
+            ShTable,
+
 
        },
-   
+
        computed: {
             confirmModal() {
                 return this.$store.getters['get_confirmForm'].confirmModal
             }
        },
-   
+
        methods: {
          editItem(item) {
          },
@@ -260,11 +174,11 @@
            }, 1000)
          }
        },
-   
+
        mounted() {
             this.getDriverList();
        },
-   
+
        watch: {
          dataTableLength() {
            this.perPageFilter = true
@@ -313,8 +227,7 @@
        }
    }
    </script>
-   
 
 
 
-  
+
