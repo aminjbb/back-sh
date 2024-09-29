2e4854581fd253f4b1a696268d070366c23b67fc
yosra <feiliyosra@gmail.com>
2024-09-29 09:57:09 +0330

    fix(DetailDiscount):add another detail and debug orderLimit

--- src/views/Voucher/DetailsDiscountCodeView.vue
+++ src/views/Voucher/DetailsDiscountCodeView.vue
@@ -20,9 +20,33 @@
               <v-divider class="mt-6"/>
             </div>
 
+            <div v-if="voucherDetail?.discount_type" class="mt-6">
+              <span class="t14 w500 mr-6">نوع مقدارتخفیف: </span>
+              <span class="t14 w500 text-gray500" v-text="voucherDetail?.discount_type === 'rial' ? 'ریالی' : 'درصدی'"/>
+              <v-divider class="mt-6"/>
+            </div>
+
+            <div v-if="voucherDetail?.voucher_type" class="mt-6">
+              <span class="t14 w500 mr-6">نوع تخفیف: </span>
+              <span class="t14 w500 text-gray500">{{voucherDetail?.voucher_type}}</span>
+              <v-divider class="mt-6"/>
+            </div>
+
+            <div v-if="voucherDetail?.name" class="mt-6">
+              <span class="t14 w500 mr-6">سازنده کد تخفیف: </span>
+              <span class="t14 w500 text-gray500"> {{voucherDetail?.name }} </span>
+              <v-divider class="mt-6"/>
+            </div>
+
             <div v-if="voucherDetail?.state?.name" class="mt-6">
               <span class="t14 w500 mr-6">بر اساس استان: </span>
               <span class="t14 w500 text-gray500"> {{voucherDetail.state?.label }} </span>
+              <v-divider class="mt-6"/>
+            </div>
+
+            <div v-if="voucherDetail?.user_limit" class="mt-6">
+              <span class="t14 w500 mr-6">تعداداستفاده مشتری: </span>
+              <span class="t14 w500 text-gray500"> {{voucherDetail?.user_limit }} </span>
               <v-divider class="mt-6"/>
             </div>
 
@@ -32,21 +56,33 @@
               <v-divider class="mt-6"/>
             </div>
 
+            <div v-if="voucherDetail?.end_time_fa" class="mt-6">
+              <span class="t14 w500 mr-6">تاریخ پایان: </span>
+              <span class="t14 w500 text-gray500"> {{voucherDetail?.end_time_fa}} </span>
+              <v-divider class="mt-6"/>
+            </div>
+
             <div v-if="voucherDetail?.max_discount" class="mt-6">
-              <span class="t14 w500 mr-6">سقف تخفیف:</span>
+              <span class="t14 w500 mr-6"> سقف تخفیف: </span>
               <span class="t14 w500 text-gray500">  {{voucherDetail?.max_discount}}</span>
               <v-divider class="mt-6"/>
             </div>
 
-            <div v-if="voucherDetail?.order_count" class="mt-6">
-              <span class="t14 w500 mr-6">تعداد سفارش:</span>
-              <span class="t14 w500 text-gray500"> {{voucherDetail?.order_count}} </span>
+            <div v-if="voucherDetail?.order_limit" class="mt-6">
+              <span class="t14 w500 mr-6"> تعداد استفاده سفارش: </span>
+              <span class="t14 w500 text-gray500"> {{voucherDetail?.order_limit}} </span>
               <v-divider class="mt-6"/>
             </div>
 
-            <div v-if="voucherDetail?.order_limit" class="mt-6">
-              <span class="t14 w500 mr-6">چندمین سفارش:</span>
-              <span class="t14 w500 text-gray500"> {{voucherDetail?.order_limit}} </span>
+            <div v-if="voucherDetail?.order_count" class="mt-6">
+              <span class="t14 w500 mr-6">  چندمین سفارش: </span>
+              <span class="t14 w500 text-gray500">  {{voucherDetail?.order_count}} </span>
+              <v-divider class="mt-6"/>
+            </div>
+
+            <div v-if="voucherDetail?.sending_price" class="mt-6">
+              <span class="t14 w500 mr-6"> هزینه ارسال: </span>
+              <span class="t14 w500 text-gray500" v-text="voucherDetail?.sending_price === 'free' ? 'رایگان' : 'پیش فرض'"/>
               <v-divider class="mt-6"/>
             </div>
 
@@ -54,6 +90,12 @@
               <span class="t14 w500 mr-6">حداقل هزینه سفارش:</span>
               <span class="t14 w500 text-gray500"> {{voucherDetail?.min_order_price}} </span>
             </div>
+
+            <div v-if="voucherDetail?.is_active" class="mt-6">
+              <span class="t14 w500 mr-6"> وضعیت کد تخفیف: </span>
+              <span class="t14 w500 text-gray500" v-text="voucherDetail?.is_active === 1 ? 'فعال' : 'غیر فعال'"/>
+            </div>
+
           </div>
         </v-card>
       </div>
